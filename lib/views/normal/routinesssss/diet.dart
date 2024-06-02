06c1a9de99c2a932fd3503e628bc0c70ef554082
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-06-02 13:16:48 +0200

    up

--- lib/views/normal/routinesssss/diet.dart
+++ lib/views/normal/routinesssss/diet.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:math';
 
 class DietRoutinePage extends StatefulWidget {
   final DateTime selectedDate;
   final String username;
 
-  DietRoutinePage({required this.selectedDate, required this.username});
+  const DietRoutinePage({required this.selectedDate, required this.username});
 
   @override
   _DietRoutinePageState createState() => _DietRoutinePageState();
@@ -21,6 +22,9 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
 
   List<Map<String, dynamic>> _meals = [];
   num _dailyCalories = 0;
+  num _dailyCarbs = 0;
+  num _dailyFat = 0;
+  num _dailyProtein = 0;
   num _monthlyCalories = 0;
 
   @override
@@ -41,6 +45,9 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
       setState(() {
         _meals = meals.map((e) => Map<String, dynamic>.from(e)).toList();
         _dailyCalories = data['dailyCalories'] ?? 0;
+        _dailyCarbs = data['dailyCarbs'] ?? 0;
+        _dailyFat = data['dailyFat'] ?? 0;
+        _dailyProtein = data['dailyProtein'] ?? 0;
       });
     }
   }
@@ -69,7 +76,7 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
     });
   }
 
-  Future<void> _updateCalories() async {
+  Future<void> _updateMeals() async {
     final docRef = FirebaseFirestore.instance
         .collection('diet_routines')
         .doc('${widget.selectedDate.toLocal()}_${widget.username}');
@@ -79,8 +86,12 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
       'date': widget.selectedDate,
       'meals': _meals,
       'dailyCalories': _dailyCalories,
-      'monthlyCalories': _monthlyCalories,
+      'dailyCarbs': _dailyCarbs,
+      'dailyFat': _dailyFat,
+      'dailyProtein': _dailyProtein,
     });
+
+    _fetchMonthlyCalories(); // Recalculate monthly calories after updating meals
   }
 
   void _addMeal() {
@@ -95,9 +106,12 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
     setState(() {
       _meals.add(meal);
       _dailyCalories += meal['calories'] as num;
+      _dailyCarbs += meal['carbs'] as num;
+      _dailyFat += meal['fat'] as num;
+      _dailyProtein += meal['protein'] as num;
       _monthlyCalories += meal['calories'] as num;
     });
-    _updateCalories();
+    _updateMeals();
     _clearForm();
   }
 
@@ -172,16 +186,21 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
                   'protein': int.parse(_proteinController.text),
                 };
                 setState(() {
-                  final oldCalories = _meals[index]['calories'];
+                  final oldMeal = _meals[index];
+                  _dailyCalories -= oldMeal['calories'] as num;
+                  _dailyCarbs -= oldMeal['carbs'] as num;
+                  _dailyFat -= oldMeal['fat'] as num;
+                  _dailyProtein -= oldMeal['protein'] as num;
+                  _monthlyCalories -= oldMeal['calories'] as num;
+
                   _meals[index] = modifiedMeal;
-                  _dailyCalories = _dailyCalories -
-                      (oldCalories as num) +
-                      (modifiedMeal['calories'] as num);
-                  _monthlyCalories = _monthlyCalories -
-                      (oldCalories as num) +
-                      (modifiedMeal['calories'] as num);
+                  _dailyCalories += modifiedMeal['calories'] as num;
+                  _dailyCarbs += modifiedMeal['carbs'] as num;
+                  _dailyFat += modifiedMeal['fat'] as num;
+                  _dailyProtein += modifiedMeal['protein'] as num;
+                  _monthlyCalories += modifiedMeal['calories'] as num;
                 });
-                _updateCalories();
+                _updateMeals();
                 Navigator.pop(context);
                 _clearForm();
               },
@@ -194,13 +213,16 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
   }
 
   void _deleteMeal(int index) {
-    final deletedMealCalories = _meals[index]['calories'];
+    final deletedMeal = _meals[index];
     setState(() {
       _meals.removeAt(index);
-      _dailyCalories -= deletedMealCalories;
-      _monthlyCalories -= deletedMealCalories;
+      _dailyCalories -= deletedMeal['calories'] as num;
+      _dailyCarbs -= deletedMeal['carbs'] as num;
+      _dailyFat -= deletedMeal['fat'] as num;
+      _dailyProtein -= deletedMeal['protein'] as num;
+      _monthlyCalories -= deletedMeal['calories'] as num;
     });
-    _updateCalories();
+    _updateMeals();
   }
 
   void _clearForm() {
@@ -214,67 +236,170 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
     });
   }
 
+  Color _getRandomColor() {
+    Random random = Random();
+    return Color.fromARGB(
+      255,
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Diet Routine'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Form(
-              child: Column(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _mealNameController,
+                      decoration: InputDecoration(labelText: 'Meal Name'),
+                    ),
+                    DropdownButtonFormField<String>(
+                      value: _mealType,
+                      items: ['Breakfast', 'Lunch', 'Dinner', 'Other']
+                          .map((type) => DropdownMenuItem(
+                                value: type,
+                                child: Text(type),
+                              ))
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          _mealType = value!;
+                        });
+                      },
+                      decoration: InputDecoration(labelText: 'Meal Type'),
+                    ),
+                    TextFormField(
+                      controller: _caloriesController,
+                      decoration: InputDecoration(labelText: 'Calories'),
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextFormField(
+                      controller: _carbsController,
+                      decoration: InputDecoration(labelText: 'Carbs'),
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextFormField(
+                      controller: _fatController,
+                      decoration: InputDecoration(labelText: 'Fat'),
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextFormField(
+                      controller: _proteinController,
+                      decoration: InputDecoration(labelText: 'Protein'),
+                      keyboardType: TextInputType.number,
+                    ),
+                    ElevatedButton(
+                      onPressed: _addMeal,
+                      child: Text('Add Meal'),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  TextFormField(
-                    controller: _mealNameController,
-                    decoration: InputDecoration(labelText: 'Meal Name'),
+                  Column(
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CircularProgressIndicator(
+                            value: _dailyCalories > 0 ? 1 : 0,
+                            backgroundColor: Colors.grey[200],
+                            color: _getRandomColor(),
+                            strokeWidth: 12,
+                          ),
+                          Text(
+                            '$_dailyCalories',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text('Calories')
+                    ],
                   ),
-                  DropdownButtonFormField<String>(
-                    value: _mealType,
-                    items: ['Breakfast', 'Lunch', 'Dinner', 'Other']
-                        .map((type) => DropdownMenuItem(
-                              value: type,
-                              child: Text(type),
-                            ))
-                        .toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        _mealType = value!;
-                      });
-                    },
-                    decoration: InputDecoration(labelText: 'Meal Type'),
+                  Column(
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CircularProgressIndicator(
+                            value: _dailyCarbs > 0 ? 1 : 0,
+                            backgroundColor: Colors.grey[200],
+                            color: _getRandomColor(),
+                            strokeWidth: 12,
+                          ),
+                          Text(
+                            '$_dailyCarbs',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text('Carbs')
+                    ],
                   ),
-                  TextFormField(
-                    controller: _caloriesController,
-                    decoration: InputDecoration(labelText: 'Calories'),
-                    keyboardType: TextInputType.number,
+                  Column(
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CircularProgressIndicator(
+                            value: _dailyFat > 0 ? 1 : 0,
+                            backgroundColor: Colors.grey[200],
+                            color: _getRandomColor(),
+                            strokeWidth: 12,
+                          ),
+                          Text(
+                            '$_dailyFat',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text('Fat')
+                    ],
                   ),
-                  TextFormField(
-                    controller: _carbsController,
-                    decoration: InputDecoration(labelText: 'Carbs'),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: _fatController,
-                    decoration: InputDecoration(labelText: 'Fat'),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: _proteinController,
-                    decoration: InputDecoration(labelText: 'Protein'),
-                    keyboardType: TextInputType.number,
-                  ),
-                  ElevatedButton(
-                    onPressed: _addMeal,
-                    child: Text('Add Meal'),
+                  Column(
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CircularProgressIndicator(
+                            value: _dailyProtein > 0 ? 1 : 0,
+                            backgroundColor: Colors.grey[200],
+                            color: _getRandomColor(),
+                            strokeWidth: 12,
+                          ),
+                          Text(
+                            '$_dailyProtein',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text('Protein')
+                    ],
                   ),
                 ],
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
+              SizedBox(height: 20),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
                 itemCount: _meals.length,
                 itemBuilder: (context, index) {
                   final meal = _meals[index];
@@ -299,8 +424,13 @@ class _DietRoutinePageState extends State<DietRoutinePage> {
                   );
                 },
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Text(
+                'Monthly Calories: $_monthlyCalories',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ),
       ),
     );
